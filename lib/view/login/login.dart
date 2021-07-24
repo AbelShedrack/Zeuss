2917bc245adec341ce26ee1ca2b81ceab3910c4e
shedrack <shedrackejeh458@gmail.com>
2021-07-25 00:20:50 +0100

    done

--- lib/view/login/login.dart
+++ lib/view/login/login.dart
@@ -2,84 +2,195 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:zeus/view/menu/menu.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key key}) : super(key: key);
 
   @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.all(20.0),
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 330.0,
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "FullName",
-                  prefixIcon: Icon(Icons.person),
-                  suffixIcon: Icon(Icons.edit),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  hintStyle: TextStyle(
-                    color: Colors.white,
-                    fontSize: 17.0,
-                  ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                stops: [0.0, 1.0],
+                begin: FractionalOffset.topCenter,
+                end: FractionalOffset.bottomCenter,
+                tileMode: TileMode.repeated,
+                colors: [
+                  Colors.orange,
+                  Colors.pinkAccent,
+                ],
+              ),
+            ),
+            height: 650.0,
+          ),
+          ListView(
+            children: [
+              Container(
+                height: 450.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Mhapy",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 28.0,
+                      ),
+                    ),
+                    SizedBox(height: 18.0),
+                    Card(
+                      elevation: 11,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      margin: EdgeInsets.only(left: 30.0, right: 30.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Username",
+                          prefixIcon: Icon(Icons.person, color: Colors.black26),
+                          suffixIcon: Icon(Icons.edit, color: Colors.black26),
+                          hintStyle: TextStyle(color: Colors.black26),
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(40.0),
+                            borderSide: BorderSide.none,
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 20.0,
+                            vertical: 16.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Card(
+                      elevation: 11,
+                      margin: EdgeInsets.only(left: 30.0, right: 30.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                          prefixIcon: Icon(
+                            Icons.security,
+                            color: Colors.black26,
+                          ),
+                          hintStyle: TextStyle(color: Colors.black26),
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(40.0),
+                            borderSide: BorderSide.none,
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 20.0,
+                            vertical: 16.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(30.0),
+                      width: double.infinity,
+                      height: 110.0,
+                      child: MaterialButton(
+                        onPressed: () => Get.to(HomePage()),
+                        elevation: 11.0,
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.white70,
+                          ),
+                        ),
+                        color: Colors.pink,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40.0),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Forgot your password",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
                 ),
               ),
-            ),
-            SizedBox(height: 18.0),
-            Container(
-              width: 330.0,
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "Email",
-                  prefixIcon: Icon(Icons.email),
-                  suffixIcon: Icon(Icons.edit),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  hintStyle: TextStyle(
-                    color: Colors.white,
-                    fontSize: 17.0,
-                  ),
+              SizedBox(height: 100.0),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text("Or Connect With"),
+                    SizedBox(height: 20.0),
+                    Row(
+                      children: [
+                        SizedBox(width: 20.0),
+                        Expanded(
+                          child: MaterialButton(
+                            child: Text("Facebook"),
+                            textColor: Colors.white,
+                            color: Colors.blue,
+                            onPressed: () {},
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(40.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 10.0),
+                        Expanded(
+                          child: MaterialButton(
+                            child: Text("Twitter"),
+                            textColor: Colors.white,
+                            color: Colors.indigoAccent.shade700,
+                            onPressed: () {},
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(40.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 20.0),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Dont have an account?"),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Sign up",
+                            style: TextStyle(
+                              color: Colors.indigo,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-            ),
-            SizedBox(height: 18.0),
-            Container(
-              width: 330.0,
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: "Password",
-                  prefixIcon: Icon(Icons.security),
-                  suffixIcon: Icon(Icons.edit),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  hintStyle: TextStyle(
-                    color: Colors.white,
-                    fontSize: 17.0,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 18.0),
-            MaterialButton(
-              onPressed: () => Get.to(MenuPage()),
-              child: Text("Create Account"),
-              textColor: Colors.white,
-              color: Colors.black,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-              ),
-            ),
-          ],
-        ),
+            ],
+          ),
+        ],
       ),
     );
   }
